477e655910fb0331afeb478e24eb895264237b5a
alicemichel <61995547+alicemichel@users.noreply.github.com>
2024-03-21 16:27:51 -0700

    Update functions_get_pull_times.R

--- functions_get_pull_times.R
+++ functions_get_pull_times.R
@@ -5,14 +5,14 @@ fullTimeFromClipStart <- function(sound.path, clip.start){
   return(startFileTime+clip.start)
 }
 
-pullTime <- function(clip.start, inPAMfile, getPAM, clipLength=len){
+pullTime <- function(clip.start, inPAMfile, getPAM, clipLength=len, plot=TRUE){
   fullTimeSecondsToGet <- fullTimeFromClipStart(sound.path = inPAMfile, clip.start = clip.start)
   dateYYYYmmdd <- substr(inPAMfile, start = 4, stop = 11)
-  return(pullFromPam(fullTimeSecondsToGet, dateYYYYmmdd, getPAM=pam, clipLength=len, path="."))
+  return(pullFromPam(fullTimeSecondsToGet, dateYYYYmmdd, getPAM, clipLength, path, plot))
 }
 
 
-pullFromPam <- function(fullTimeSecondsToGet, dateYYYYmmdd, getPAM=pam, clipLength=len, path="."){
+pullFromPam <- function(fullTimeSecondsToGet, dateYYYYmmdd, getPAM, clipLength, path=".", plot=TRUE){
   
   oldw <- getOption("warn")
   options(warn = -1)
@@ -42,7 +42,9 @@ pullFromPam <- function(fullTimeSecondsToGet, dateYYYYmmdd, getPAM=pam, clipLeng
   }
   
   tmpls <- list(getPAM, kfl, bts, read_wave(kfl, from = bts, to = bts + len))
-  viewSpec(tmpls[[4]], main = pam, frq.lim = c(0.1, 0.9), wl=2048, ovlp=99, wn="hanning")
+  if (plot==TRUE){
+    viewSpec(tmpls[[4]], main = pam, frq.lim = c(0.1, 0.9), wl=2048, ovlp=99, wn="hanning")
+  }
   return(tmpls)
   
   options(warn = oldw)
@@ -51,7 +53,7 @@ pullFromPam <- function(fullTimeSecondsToGet, dateYYYYmmdd, getPAM=pam, clipLeng
 
 mergedClipsFromTimeGaps <- function(clip.start, inPAMfile, gaps, getPAM, clipLength=len, path="."){
   
-  pam.1st.clip <- pullTime(clip.start, inPAMfile, getPAM, clipLength)
+  pam.1st.clip <- pullTime(clip.start, inPAMfile, getPAM, clipLength, plot=FALSE)
   first.file.start <- fullTimeFromClipStart(pam.1st.clip[[2]],0)
   first.clip.start <- fullTimeFromClipStart(pam.1st.clip[[2]],pam.1st.clip[[3]])
   all.clip.starts <- first.clip.start + gaps
@@ -64,8 +66,7 @@ mergedClipsFromTimeGaps <- function(clip.start, inPAMfile, gaps, getPAM, clipLen
   daytext = substr(kfls, start = 4, stop = 11) 
   kfls = kfls[daytext == dateYYYYmmdd] # exclude other days -- could be a problem for midnight to 1am
   kfls = kfls[nchar(kfls)==81] # exclude cut files from processing.
-  cat("note: files must have the format letter_file!")
-  
+
   fls <- kfls[rems]
   all.clip.starts1 <- all.clip.starts - fullTimeFromClipStart(fls,0)
   
@@ -75,6 +76,11 @@ mergedClipsFromTimeGaps <- function(clip.start, inPAMfile, gaps, getPAM, clipLen
   }
   return(do.call(tuneR::bind, args = clps))
 }
+
+
+
+
+
 
 
 
